2b488829f84d6ad5b48aa4b7df4c52e8b12078fc
automationdesign <157027707+automationdesign@users.noreply.github.com>
2024-04-06 21:39:04 +0000

    Pending changes exported from your codespace

---
+++ code.ts
@@ -0,0 +1,64 @@
+createCover().then(() => {
+    figma.closePlugin();
+  });
+  
+  async function createCover() {
+    // Создаем пустой фрейм
+    const coverFrame = figma.createFrame();
+  
+    // Задаем размер
+    coverFrame.resize(1920, 1080);
+  
+    // Добавляем фрейм на страницу
+    figma.currentPage.appendChild(coverFrame);
+  
+    // Создаем текстовый блок для заголовка
+    const titleText = figma.createText();
+  
+    // Загружаем шрифт для заголовка
+    await figma.loadFontAsync({ family: "IBM Plex Mono", style: "Bold" });
+  
+    // Задаем шрифт для блока текста
+    titleText.fontName = { family: "IBM Plex Mono", style: "Bold" };
+  
+    // Задаем размер шрифта и координаты
+    titleText.fontSize = 136;
+    titleText.x = 100;
+    titleText.y = 100;
+  
+    // Задаем текст для блока, берем его из заголовка документа
+    titleText.characters = figma.root.name;
+  
+    // Добавляем текстовый блок на страницу
+    coverFrame.appendChild(titleText);
+  
+    // Добавляем имя пользователя
+    const authorText = figma.createText();
+  
+    // Загружаем шрифт для заголовка
+    await figma.loadFontAsync({ family: "IBM Plex Mono", style: "Regular" });
+  
+    // Задаем шрифт для блока текста
+    authorText.fontName = { family: "IBM Plex Mono", style: "Regular" };
+  
+    // Задаем текст для автора файла, берем имя текущего пользователя
+    let authorName = "John Wick";
+  
+    if (figma.currentUser?.name) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      authorName = figma.currentUser.name;
+    }
+  
+    authorText.characters = `Автор: ${authorName}`;
+  
+    // Задаем размер шрифта и координаты
+    authorText.fontSize = 44;
+    authorText.x = 100;
+    authorText.y = 100 + 200 + 8;
+  
+    // Добавляем текстовый блок на страницу
+    coverFrame.appendChild(authorText);
+  
+    await figma.setFileThumbnailNodeAsync(coverFrame);
+  }
+  
